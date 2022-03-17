fbcc53bca2dc984b12cee980df19b3f21d234384
alberteinstein42 <gdrive.storage.15gb@gmail.com>
2022-03-17 22:35:32 +0530

    Bot UI + Config moved

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const botController = require("./src/bot-controller");
 
+//Loading Configuration from Config Vars/Environment Variables
+const PORT = process.env.PORT || 80;
+const token = process.env.BOT_TOKEN;
+var msgsuite_ng_config = JSON.parse(process.env.MSGSUITE_NG_CONFIG);
+if(!msgsuite_ng_config){ console.log("Error loading MSGSUITE_NG_CONFIG, make sure the Config Var is properly set."); process.exit(1); }
 
-//REST Server
+
+
+
+//poor man's key-value pair database
+//this might be cleared at every dyno restart and possible app crash
+//but the bot is not critical, and can work with some working data loss
+//worst case scenario - bot will ask all the details, again.
+global.database = { user: []}; 
+global.user_init = { state: 'INIT', current_domain_page: 1, data: { email: undefined, td_name: undefined, domain: undefined } };
+global.msgsuite_ng_config = msgsuite_ng_config;
+
+
+//REST Server - for status checking and prevent idling
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
@@ -17,17 +34,21 @@ app.get('/', (req, res) => {
     res.send({"status": "ok"});
 });
 
-const PORT = process.env.PORT || 80;
+
 app.listen(PORT, () => {
     console.log('listening on port '+ PORT);
 });
 
 
 //Telegram Bot Server
-const token = process.env.BOT_TOKEN; //save config vars in settings of the heroku project
 const bot = new TelegramBot(token, {polling: true});
 
 
 bot.on('message', (msg) => {
-    botController.handle(bot, msg);
+    botController.message(bot, msg);
+});
+
+
+bot.on('callback_query', (msg) => {
+    botController.callback_query(bot, msg);
 });
--- src/bot-controller.js
+++ src/bot-controller.js
@@ -1,13 +1,134 @@
 module.exports = {
 
-	handle: (bot, msg) => {
-		const chatId = msg.chat.id;
-    	bot.sendMessage(chatId, "Received"
-    		{
-    			reply_markup:[{
-    				"text": "Generate TD",
-    				"callback_data": "generate_td"
-    			}]
-    		});
+	message: (bot, msg) => {
+		//console.log(msg);
+		try{
+			var user = global.database.user[msg.from.id];
+			if(!user){user = global.user_init; }
+			handleMessage(bot, msg, user);
+		}catch(ex){
+			console.error(ex);
+		}    		
+	},
+
+	callback_query: (bot, msg) => {
+		//console.log(msg);
+		
+		var user = global.database.user[msg.from.id];
+		if(!user){user = global.user_init; }
+
+		handleCallbackQuery(bot, msg, user);
+		
 	}
 };
+
+function handleCallbackQuery(bot, msg, user){
+	const chatId = msg.message.chat.id;
+
+	switch(msg.data){
+		case 'ENTER_EMAIL':
+			bot.editMessageText("Email Address? -", {
+				message_id: msg.message.message_id,
+				chat_id: chatId
+			});
+
+			user.state = 'ENTER_EMAIL';
+			break;
+		case 'GO_TO_FIRST_PAGE':
+			break;
+		case 'GO_TO_PREV_PAGE':
+			break;
+		case 'GO_TO_NEXT_PAGE':
+			break;
+		case 'GO_TO_LAST_PAGE':
+			break;
+
+
+		default:
+
+	}
+
+	//Handling the dynamic SELECTED_DOMAIN_XX separately
+	if(msg.data.startsWith("SELECTED_DOMAIN_")){
+		var selectedDomain = msg.data.split("SELECTED_DOMAIN_")[1];
+		bot.editMessageText("Processing your request to create TD on Domain #" + selectedDomain, {
+			message_id: msg.message.message_id,
+			chat_id: chatId
+		});
+	}
+}
+
+function validateEmail(emailAdress){
+	let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (emailAdress.match(regexEmail)) {
+		return true; 
+	} else {
+		return false; 
+	}
+}
+
+function handleMessage(bot, msg, user){
+	const chatId = msg.chat.id;
+
+	switch(user.state){
+		case 'ENTER_EMAIL':
+			if(!validateEmail(msg.text.trim())){
+				bot.sendMessage(chatId, "Invalid Email Address, please provide a valid email. - ");	
+				break;
+			}
+			user.state = "ENTER_TD_NAME";
+			user.data.email = msg.text.trim();
+			bot.sendMessage(chatId, "Drive Name? - ");
+			break;
+
+		case 'ENTER_TD_NAME':
+			user.state = "SELECT_DOMAIN";
+			user.data.td_name = msg.text.trim();
+			
+			var inline_keyboard = generateDomainsMarkup(user);			
+			bot.sendMessage(chatId, "Select Domain - ", {
+				reply_markup: '{ "inline_keyboard": '+ JSON.stringify(inline_keyboard) + ' }'
+			});
+			break;
+
+		default:
+			var inline_keyboard = [
+				[{"text": "💽 Generate TD", "callback_data": "ENTER_EMAIL"}], 
+				[{"text": "❌ Cancel", "callback_data": "CANCEL"}]
+			]; 
+			bot.sendMessage(chatId, "Hi, welcome to MsGSuite-NG.", {
+				reply_markup: '{ "inline_keyboard": '+ JSON.stringify(inline_keyboard) + ' }'
+			});
+	}
+}
+
+
+function generateDomainsMarkup(user){
+	var domains = global.msgsuite_ng_config.domains;
+	var bottom_controls = [{"text": "⏮", "callback_data": "GO_TO_FIRST_PAGE"}, {"text": "⏪", "callback_data": "GO_TO_PREV_PAGE"}, {"text": "⏩", "callback_data": "GO_TO_NEXT_PAGE"}, {"text": "⏭", "callback_data": "GO_TO_LAST_PAGE"}];
+	var cancel_control = [{"text": "❌ Cancel", "callback_data": "CANCEL"}];
+
+	
+	var inline_keyboard = generateDomainsPage(user.current_domain_page);
+	inline_keyboard.push(bottom_controls);
+	inline_keyboard.push(cancel_control);
+	return inline_keyboard;
+}
+
+function generateDomainsPage(pageNo){
+	pageNo = pageNo - 1; // To make the code easy to read, pages and domain indices start from 1
+	var domains = global.msgsuite_ng_config.domains;
+	var currentPageLength = Math.min(domains.length - (pageNo * 10), 10);
+	var emoji_numbers = ["0⃣", "1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"];
+
+	var pageArr = [[{"text": "🎲 Random", "callback_data": "SELECTED_DOMAIN_RANDOM"}]];
+	for(i=0; i<currentPageLength; i++){
+		var domainIndex = pageNo*10 + i;
+		var domain = domains[domainIndex];
+		pageArr.push([
+			{"text": emoji_numbers[domainIndex+1] + " " + domain.name, "callback_data": "SELECTED_DOMAIN_" + (domainIndex+1)}
+		]);
+	}
+
+	return pageArr;
+}
